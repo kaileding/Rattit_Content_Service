d9e5cbd5dfae8c13730b611d62d1e8ddc7f41539
kaileding <dingkaile@gmail.com>
2017-05-27 16:05:07 -0700

    init node project

---
+++ src/app.js
@@ -0,0 +1,10 @@
+/*
+* @Author: KaileDing
+* @Date:   2017-05-27 16:01:43
+* @Last Modified by:   kaileding
+* @Last Modified time: 2017-05-27 16:03:50
+*/
+
+'use strict';
+
+import express from 'express'
