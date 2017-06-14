2a24eec31d6d706cedbee5ba3b73a781d057a51b
kaileding <dingkaile@gmail.com>
2017-06-14 01:00:07 -0700

    remove commented code

--- src/routes/DBRoutes.js
+++ src/routes/DBRoutes.js
@@ -2,7 +2,7 @@
 * @Author: KaileDing
 * @Date:   2017-06-05 14:02:16
 * @Last Modified by:   kaileding
-* @Last Modified time: 2017-06-14 00:54:29
+* @Last Modified time: 2017-06-14 00:59:04
 */
 
 'use strict';
@@ -22,36 +22,29 @@ router.get('/init', (req, res, next) => {
 	
     cLogger.say(cLogger.TESTING_TYPE, 'start');
 
-    // models.Users.sync({
-    //     force: true
-    // }).then(r => {
-        dbConnectionPool.sync({
-            force: true
-        }).then(function (r) {
-            if (forceFlag) {
-                dbInitialization().then(result => {
-                    res.status(httpStatus.OK).send({
-                        status: "success",
-                        message: 'Database forcely synchronized successfully with testing data.'
-                    });
-                }).catch(error => {
-                    cLogger.say(cLogger.ESSENTIAL_TYPE, error);
-                    next(new APIError('Failed to insert testing data into database.'));
-                });
-            } else {
+    dbConnectionPool.sync({
+        force: true
+    }).then(function (r) {
+        if (forceFlag) {
+            dbInitialization().then(result => {
                 res.status(httpStatus.OK).send({
                     status: "success",
-                    message: 'Database synchronized successfully'
+                    message: 'Database forcely synchronized successfully with testing data.'
                 });
-            }
-        }, function (err) {
-            cLogger.say(cLogger.ESSENTIAL_TYPE, err);
-            next(err);
-        });
-    // }).catch(err => {
-    //     cLogger.say(cLogger.ESSENTIAL_TYPE, err);
-    //     next(err);
-    // });
+            }).catch(error => {
+                cLogger.say(cLogger.ESSENTIAL_TYPE, error);
+                next(new APIError('Failed to insert testing data into database.'));
+            });
+        } else {
+            res.status(httpStatus.OK).send({
+                status: "success",
+                message: 'Database synchronized successfully'
+            });
+        }
+    }, function (err) {
+        cLogger.say(cLogger.ESSENTIAL_TYPE, err);
+        next(err);
+    });
 	
 });
 
